5baec7c03d7b001347eda9459247684f9499ef5f
varun27896 <varun27896@gmail.com>
2020-06-08 20:47:47 -0500

    iterative and recursive method to find the closest value to the target in a BST

--- closestvalue_bst/closest_target.py
+++ closestvalue_bst/closest_target.py
@@ -17,3 +17,20 @@ def closestBSTHelper(tree, target, closest):
         return closestBSTHelper(tree.right, target, closest)
     else:
         return closest
+
+#iterative method
+def closestBst(tree, target):
+    return closestBstHelper(tree, target, float("inf"))
+
+def closestBstHelper(tree, target, closest):
+    while tree is not None:
+        currenNode = tree
+        if abs(target - closest) > abs(target - currentNode.value):
+            closest = currentNode.value
+        if target < currentNode.value:
+            currentNode = currenNode.left
+        elif target > currentNode.value:
+            currenNode = currentNode.right
+        else:
+            break
+    return closest 
