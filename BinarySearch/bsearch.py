6ea6eaab1b78f00c49a75b6377d13d9c5d037435
varun27896 <varun27896@gmail.com>
2020-06-16 17:40:24 -0500

    recursive and iterative approach

---
+++ BinarySearch/bsearch.py
@@ -0,0 +1,5 @@
+#Write a function that takes in a sorted array of integers and a target integer
+#and returns the index of the number in the array if it matches with the target
+
+def binarySearch(array, target):
+    def binarySearchHelper(array, target, 0, len(array) - 1)
