fc98506052a71adb996504296d3823ca31e27f1c
varun27896 <varun27896@gmail.com>
2020-07-21 11:46:31 -0500

    o(n) 0(1)

---
+++ Strings/palindrome.py
@@ -0,0 +1,10 @@
+def isPalindrome(string):
+    leftIdx = 0
+    rightIdx = len(string) - 1
+    while leftIdx < rightIdx:
+        if string[leftIdx] != string[rightIdx]:
+            return False
+        leftIdx += 1
+        rightIdx -= 1
+    return True
+#O(n) time complexity | O(1) space complexity
